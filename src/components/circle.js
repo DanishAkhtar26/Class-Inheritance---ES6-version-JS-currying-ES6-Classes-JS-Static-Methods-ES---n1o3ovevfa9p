1f5d78154437ead20e15b1bc4a469077fe5a9e3e
DanishAkhtar26 <102619612+DanishAkhtar26@users.noreply.github.com>
2022-10-18 01:55:23 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,21 @@
 
 
 // export class using module.exports
+// import using require
+
+// declare class
+
+
+// export class using module.exports
+var Shape = require('./shape.js');
+
+class Circle extends Shape {
+     constructor(name) {
+          super(name);
+     }
+     calculateArea(radius) {
+         return Math.PI * Math.pow(radius,2);
+     }
+}
+
+module.exports = Circle;
